6832d9cc957288382934a05043a2e2a20d978e09
Eucharia Alaguva <euchariaalaguva@Eucharias-MacBook-Air.local>
2023-05-04 20:32:25 +0100

    new changes

--- cellregistration/urls.py
+++ cellregistration/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cellregistrations/', include('cellregistrations.urls')),
+    path('', include('cellregistrations.urls')),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
